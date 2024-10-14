5c797e61cbfb1eeaad22911700f949492b8ce3cd
Nima Xin <nazirnasiri786@gmail.com>
2024-10-14 02:02:58 +0200

    Chore: Bump version to 0.2.4

--- src/init_data_py/errors/errors.py
+++ src/init_data_py/errors/errors.py
@@ -1,23 +1,27 @@
-class AuthDateMissingError(Exception):
+class InitDataPyError(Exception):
+    pass
+
+
+class AuthDateMissingError(InitDataPyError):
     def __init__(self):
         super().__init__("auth_date is missing.")
 
 
-class ExpiredError(Exception):
+class ExpiredError(InitDataPyError):
     def __init__(self):
         super().__init__("init data is expired.")
 
 
-class SignInvalidError(Exception):
+class SignInvalidError(InitDataPyError):
     def __init__(self):
         super().__init__("signature (hash) is invalid.")
 
 
-class SignMissingError(Exception):
+class SignMissingError(InitDataPyError):
     def __init__(self):
         super().__init__("signature (hash) is missing.")
 
 
-class UnexpectedFormatError(Exception):
+class UnexpectedFormatError(InitDataPyError):
     def __init__(self):
         super().__init__("the init data query string has unexpected format.")
